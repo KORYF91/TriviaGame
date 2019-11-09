894e612e36d89a069191154f4f4a9ea47c508b49
Kfriedman <Koryf91@gmail.com>
2019-11-08 23:33:01 -0800

    set the t&f questions and set up the reset but to refresh the whole quiz

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TriviaGame
+Here we have a trivia game. 
+
+The user will have 180second to complete 20 questions that will be true or false. 
+The are going to be in the theme of 90's trivia. 
+The user will begin the game by pressing the start button. 
+The timer will begin and questions will then appear. 
+The user will have 180 seconds (3 minutes to complete)
+Once the user has answered all twenty questions the user can push the done button to see how they did. if the user cant answer all questions before the time is up they will the scoring will happen automatically.
+SPECIAL THANKS TO BUZZ FEED FOR THE THE FUN RANDOM TRIVIA.
+ONLY A TRUE FAN OF THE 90'S WILL BE ABLE TO PASS THIS TEST. 
+[trivia pulled from ]https://www.buzzfeed.com/briangalindo/the-one-where-we-found-out-jennifer-aniston-hated-the-rachel
--- assest/javascript/script.js
+++ assest/javascript/script.js
@@ -10,16 +10,106 @@ var incorrect = 0;
 //THERE ARE ABOUT 20 QUESTIONS LEFT TO ENTER WITH VALUES.
 var questions = [
     {
-        question: "color of sky?",
-        choices: ['blue', 'red', 'green'],
-        correctAnswer: 'blue'
+        question: "Y2K was believed to be the era of peace and harmony with techonolgy?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
     },
     {
-        question: "color of sky?",
-        choices: ['blue', 'red', 'green'],
-        correctAnswer: 'blue'
-    }
-
+        question: "Rocko from Rocko’s Modern Life and the Spongebob square pants are both voiced by the same voice actor?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
+    },
+    {
+        question: "Michael Jackson sang backup vocals on “Do the Bartman.”?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "jennifer Aniston HATED her iconic 'The Rachel' haircut.?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Backstreet Boys got their name from Justin Timberlakes mom?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FLASE.'
+    },
+    {
+        question: "The Return of Jafar was Disney's first direct-to-video sequel?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Totally Hair Barbie's hair was 1 foot long?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FLASE'
+    },
+    {
+        question: "Beanie Babies were a success from their launch?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
+    },
+    {
+        question: "The Rugrats have a star on the Hollywood Walk of Fame ?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "n 1999 the NSA banned Furbies from its headquarters in Maryland, over fears that they would hear top-secret conversations and then repeat them.?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Tamagotchi is a portmanteau of the Japanese words for egg and watch.",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Clarissa Explains It All almost got a spinoff series?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
+    },
+    {
+        question: "There is a banned episode of Tiny Toons where Buster, Hamton, and Plucky get drunk, steal a police car, and die in a drunk driving accident?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Marlon Wayans gets residuals for Batman Forever, even though he does NOT appear in the movie?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Jessie was originally supposed to be addicted to speed in that classic episode of Saved by the Bell?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
+    },
+    {
+        question: "JNCO is an abbreviation of 'judge none, choose one'",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Family Matters is actually a spinoff of Perfect Strangers?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "In 1991, DuckTales became the first American cartoon series to be shown in the Soviet Union since the start of the Cold War?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Harrison Ford turned down Steven Spielberg's offer for him to play Dr. Alan Grant in Jurassic Park?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'TRUE'
+    },
+    {
+        question: "Nintendo intended to release another gaming console?",
+        choices: ['TRUE', 'FALSE'],
+        correctAnswer: 'FALSE'
+    },
+  
 ]
 // THIS FUNCTION BEGIN THE TIMER ALONG WITH SHOWING THE QUESTIONS AND ANSWERS TO THE ARRAY.
 $(document).on("click", "#start", function () {
@@ -36,7 +126,7 @@ $(document).on("click", "#start", function () {
 $(document).on("click", "#done", function () {
     //use for loop to check answers.
     for (var i = 0; i < questions.length; i++) {
-        //THIS FUNCTION IS RUNNING THROUGH THE INPUT FROM THE USER AND "MARKING CORRECT OR INCORRECT"
+        //THIS METHOD IS RUNNING THROUGH THE INPUT FROM THE USER AND "MARKING CORRECT OR INCORRECT"
         $.each($("input[name='question-" + i + "']:checked"), function () {
             if ($(this).val() === questions[i].correctAnswer) {
                 correct++
@@ -58,11 +148,11 @@ $(document).on("click", "#done", function () {
     // })
     showResults();
 })
-// HERE WE USE THE DATA PASSED THE VAR CORRECT AND INCORRECT TO DISPLAY HOW THE USER DID. 
+// HERE THE DATA PASSED TO THE VARIABLE CORRECT AND INCORRECT TO DISPLAY HOW THE USER DID. 
 // I NEED TO ADD TO THE FUNCTION TO INCOPERATE ONCE THE TIMER IS DONE THE PAGE IS AUTOMATICALLY SCORED. AND RESULTS DISPLAYED.
 function showResults() {
-    $("#quiz-area").html("<h2>Corrects: " + correct + "</h2>");
-    $("#quiz-area").append("<h2> incorrects: " + incorrect + "</h2>")
+    $("#quiz-area").html("<h2>Correct: " + correct + "</h2>");
+    $("#quiz-area").append("<h2> incorrect: " + incorrect + "</h2>")
 }
 //THIS HOW THE TIMER IS WORKING. THE COUNTER VAR IS DECREMENTING AND LINE 66 IS HOW THIS IS DISPLAYING ON THE PAGE.
 function countdown() {
@@ -79,8 +169,18 @@ $(document).on("click", "#stop", function () {
     $("#timer").html(counter);
 })
 // THIS FUNCTION ONLY RESETS THE TIMER, THIS NEEDS TO RESRT THE ENTIRE PAGE. 
-$(document).on("click", "#restart", function () {
-    counter = 50;
+$(document).on("click", "#reset", function () {
+    counter =  180;
     $("#timer").html(counter);
+
+    timer = setInterval(countdown, 1000);
+    for (var i = 0; i < questions.length; i++) {
+        $("#quiz-area").append("<h2>" + questions[i].question + "</h2>")
+        for (var j = 0; j < questions[i].choices.length; j++) {
+            $("#quiz-area").append("<input type='radio' name='question-" + i + "'value ='" + questions[i].choices[j] + "''>" + questions[i].choices[j]);
+        }
+    }
+    $("#quiz-area").append("<button id='done'>Done</button>");
 })
+
 
